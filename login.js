9addb45d567e7d631a4f0d5cbadfb9320a0856ef
Sauma Saha <SaumaSaha@users.noreply.github.com>
2023-09-15 11:22:50 +0530

    Add auto-login for cluedo

---
+++ login.js
@@ -0,0 +1,33 @@
+const puppeteer = require("puppeteer");
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--start-maximized"],
+  });
+
+  browser.on("targetdestroyed", () => {
+    console.log("Browser closed");
+    browser.close();
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto("http://localhost:8000");
+
+  await page.setViewport({ width: 1792, height: 920 });
+
+  await page.click("a");
+
+  await delay(2000);
+
+  await page.click("#join-game-btn");
+
+  await delay(2000);
+
+  await page.type("#join-game-input-name", process.argv[2]);
+
+  await page.click("#join-game-confirm-btn");
+})();
---
+++ test.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+node login.js "sauma" &
+
+node login.js "milan" &
+
+node login.js "riya" &
